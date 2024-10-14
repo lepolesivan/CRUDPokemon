cec2891c890cc9707c8b9120330a5e3396f9b425
lepolesivan <ivansomo27@gmail.com>
2024-10-14 14:33:11 +0200

    Création du répo

---
+++ src/Views/home.view.php
@@ -0,0 +1,9 @@
+<?php
+require_once(__DIR__ . '/partials/head.php');
+?>
+<div class="container">
+    <h1>Pokemon</h1>
+</div>
+<?php
+require_once(__DIR__ . '/partials/footer.php');
+?>
---
+++ src/Views/partials/footer.php
@@ -0,0 +1,9 @@
+</main>
+<footer class="text-center text-light bg-black">
+    <div class="text-center p-3">
+        © Copyright : nanaplouf <?php echo date("Y") ?>
+    </div>
+</footer>
+</body>
+
+</html>
